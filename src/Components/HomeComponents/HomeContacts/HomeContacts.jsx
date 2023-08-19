4d691e89420b080c062941b10d93de8d69ead7d1
Sanjeev <94432263+sanjeev662@users.noreply.github.com>
2023-08-19 15:26:41 +0530

    contact height

--- src/Components/HomeComponents/HomeContacts/HomeContacts.jsx
+++ src/Components/HomeComponents/HomeContacts/HomeContacts.jsx
@@ -3,7 +3,7 @@ import Form from "./Form";
 
 function Contacts() {
   return (
-    <section className="d-flex flex-column container" style={{paddingBottom:"35px",paddingTop:"0px"}}>
+    <section className="d-flex flex-column container" style={{paddingBottom:"35px",paddingTop:"5px",minHeight:"80vh"}}>
        <div className="section-title padd-15">
         <h2>Contact Me</h2>
       </div>
